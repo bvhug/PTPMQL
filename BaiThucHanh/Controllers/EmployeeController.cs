e8c3b815cbfd799fae76a6f0a12e3d9e879eeff4
bvhug <trvhung168@gmail.com>
2024-02-28 15:00:19 +0700

    #7 Slide 17

--- BaiThucHanh/Controllers/EmployeeController.cs
+++ BaiThucHanh/Controllers/EmployeeController.cs
@@ -1,4 +1,17 @@
+using Microsoft.AspNetCore.Mvc;
+
 namespace BaiThucHanh.Controllers;
-{
-    
-}
+    public class EmployeeController : Controller
+    {
+         public IActionResult Index()
+    {
+        return View();
+    }
+    [HttpPost]
+    public IActionResult Index(string EmployeeID)
+    {
+        string strResult = "Xin chao" + EmployeeID ;
+        ViewBag.Thongbao = strResult;
+        return View();
+    }
+    }
